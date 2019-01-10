fda82ddad4300316f42e3fd75ad10f8db4480358
abdulla ayyad <abdullaayyad96@gmail.com>
2019-01-10 16:43:44 +0000

    controller fine tuning

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -22,8 +22,8 @@ class Controller(object):
         self.decel_limit = decel_limit
         
         #create throttle PID object
-        kp = 0.4
-        kd = 0.0
+        kp = 0.35
+        kd = 0.1
         ki = 0.05
         min = 0
         max = 0.3
@@ -46,16 +46,17 @@ class Controller(object):
         
                
         filtered_velocity = self.vel_lpf.filt(current_vel)
-        linear_vel_error = linear_vel_cmd - filtered_velocity
+        linear_vel_error = linear_vel_cmd - current_vel
+        filtered_error = self.error_lpf.filt(linear_vel_error)
         
         steering = self.yaw_controller.get_steering(linear_vel_cmd, angular_vel_cmd, filtered_velocity)
-        throttle = self.throttle_PID.step(linear_vel_error, self.ts)
+        throttle = self.throttle_PID.step(filtered_error, self.ts)
         brake = 0
         
-        if linear_vel_cmd == 0 and linear_vel_error < 0.05:
+        if linear_vel_cmd == 0 and filtered_error < 0.1:
             throttle = 0
             brake = 700
-        elif linear_vel_error < 0.0:
+        elif filtered_error < -0.1:
             throttle = 0
             decel = max(3*linear_vel_error, self.decel_limit)
             brake = abs(decel) * self.vehicle_mass * self.wheel_radius   
