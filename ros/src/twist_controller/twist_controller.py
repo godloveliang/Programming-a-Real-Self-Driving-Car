0418b21882715577660bbc4c034f820a8109c8ef
abdulla ayyad <abdulla.ayyad96@gmail.com>
2019-01-13 15:50:10 +0000

    seperate filtering of error and velocity

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -30,9 +30,10 @@ class Controller(object):
         self.throttle_PID = PID(kp, ki, kd, min, max)
         
         #create velocity low pass object
-        tau = 0.1
-        self.vel_lpf = LowPassFilter(tau, self.ts)
-        self.error_lpf = LowPassFilter(tau, self.ts)
+        tau_vel = 0.1
+        tau_err = 0.3
+        self.vel_lpf = LowPassFilter(tau_vel, self.ts)
+        self.error_lpf = LowPassFilter(tau_err, self.ts)
         
         #create yaw controller object
         self.yaw_controller = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
@@ -51,18 +52,18 @@ class Controller(object):
         
         steering = self.yaw_controller.get_steering(linear_vel_cmd, angular_vel_cmd, filtered_velocity)
         throttle = self.throttle_PID.step(filtered_error, self.ts)
-        brake = 0
+        brake = 0.0
         
-        if linear_vel_cmd == 0 and filtered_error < 0.1:
-            throttle = 0
+        if linear_vel_cmd == 0.0 and filtered_error < 0.1:
+            throttle = 0.0
             brake = 700
-        elif filtered_error < -0.1:
-            throttle = 0
-            decel = max(3*linear_vel_error, self.decel_limit)
+        elif filtered_error < -0.05:
+            throttle = 0.0
+            decel = max(linear_vel_error, self.decel_limit)
             brake = abs(decel) * self.vehicle_mass * self.wheel_radius   
         elif throttle < 0.05:
-            throttle = 0
-            brake = 0
+            throttle = 0.0
+            brake = 0.0
         
         return throttle, brake, steering
             
